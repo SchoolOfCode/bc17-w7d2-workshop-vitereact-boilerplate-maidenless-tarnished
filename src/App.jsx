b6e04c5929f3593dfd3f6e650b389e959e62dec3
James O'Kane <orbiccode@gmail.com>
2024-08-13 14:09:51 +0100

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./Header/header.jsx"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <header> <h1>🔥Fireplace Palace</h1></header>
+    <Header className="header" />
+    {/* <header> <h1>🔥Fireplace Palace</h1></header> */}
     <div className="wrapper">
         <main>
             <section className="hero">
---
+++ src/Header/header.jsx
@@ -0,0 +1,11 @@
+// import React
+// export default
+// funtion name with capital
+// return () for different lines 
+ import React from 'react'
+
+ export default function Header() {
+    return (
+        <h1 className='header'>🔥 Fireplace Palace</h1>
+    );
+ }
